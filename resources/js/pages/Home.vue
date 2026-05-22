e832d966d3950fcd4501b9e7d13ae14ff988bed0
P2RM <pierrymar1130@gmail.com>
2026-05-22 16:02:23 +0200

    infomaniak git synchro

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,13 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    document.title = 'Trophée de la générosité — HUGGG'
+})
+</script>
+
 <template>
     <div class="min-h-screen flex items-center justify-center">
-        <h1 class="text-3xl font-bold">Trophée de la générosité — HUG</h1>
+        <h1 class="text-3xl font-bold">Trophée de la générosité — HUGGG</h1>
     </div>
 </template>
