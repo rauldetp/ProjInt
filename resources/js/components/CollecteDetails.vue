a5e8d8cc2c9ad832bc82d172bd3d2dd61eab21de
P2RM <pierrymar1130@gmail.com>
2026-05-28 09:28:35 +0200

    add date_fin collectedetails.vue

--- resources/js/components/CollecteDetails.vue
+++ resources/js/components/CollecteDetails.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="collecte-details">
     <p>Date : {{ collecte.date_debut }}</p>
+    <p>Date fin : {{ collecte.date_fin }}</p>
     <p>Lieu : {{ collecte.lieu }}</p>
     <p>Objectif : {{ collecte.nb_inscrits_estime }} dons</p>
     <p>{{ compteur }} collègues participent déjà</p>
