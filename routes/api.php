2f4c2f0970b0668250dbd8facd044feab918f61e
P2RM <pierrymar1130@gmail.com>
2026-06-01 11:20:07 +0200

    API admin

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EntrepriseController;
 use App\Http\Controllers\CollecteController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminCollecteController;
 
 // Public
 Route::get('/entreprises/{slug}', [EntrepriseController::class, 'show']);
@@ -14,4 +16,16 @@ Route::post('/collectes/{collecte}/nb_inscrits_estime', [CollecteController::cla
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+});
+
+// Admin uniquement
+Route::middleware(['auth:sanctum', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/stats', [AdminController::class, 'stats']);
+    Route::get('/entreprises', [AdminController::class, 'entreprises']);
+
+    Route::get('/collectes', [AdminCollecteController::class, 'index']);
+    Route::post('/collectes', [AdminCollecteController::class, 'store']);
+    Route::put('/collectes/{collecte}', [AdminCollecteController::class, 'update']);
+    Route::delete('/collectes/{collecte}', [AdminCollecteController::class, 'destroy']);
+    Route::patch('/collectes/{collecte}/statut', [AdminCollecteController::class, 'updateStatut']);
 });
